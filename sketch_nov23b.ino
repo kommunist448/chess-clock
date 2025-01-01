45d9e0ebb613bdde040513615e6d131022fab617
Ufimtsev Simeon <jugomagnum@gmail.com>
2025-01-01 20:18:19 +0200

    v1.1

--- sketch_nov23b.ino
+++ sketch_nov23b.ino
@@ -1,3 +1,9 @@
+#include <GyverTM1637.h>
+
+/* Code by kommunist448,
+   with love.
+                          */
+
 #define CLK_1 2
 #define DIO_1 3
 #define BTN_1 4
@@ -6,77 +12,76 @@
 #define BTN_4 8
 #define CLK_2 6
 #define DIO_2 7
-#define zum 10
+#define zoomPin 10
 
 int MIN = 0;
 int SEC = 0;
 int MIN1 = 0;
 int SEC1 = 0;
-int finish_min = 0;
-int finish_sec = 0;
+int finishMin = 0;
+int finishSec = 0;
+
 bool bt1 = false;
 bool bt2 = false;
-bool config_mode = true;
-#include "GyverTM1637.h"
+bool configMode = true;
+
 GyverTM1637 disp1(CLK_1, DIO_1);
 GyverTM1637 disp2(CLK_2, DIO_2);
-uint32_t Now, clocktimer;
-boolean flag;
 
-void setup(){
- pinMode(BTN_1, INPUT);
- pinMode(BTN_2, INPUT);
- pinMode(BTN_3, INPUT);
- pinMode(BTN_4, INPUT);
- pinMode(zum, OUTPUT);
- Serial.begin(9600);
+
+void setup() {
+  pinMode(BTN_1, INPUT);
+  pinMode(BTN_2, INPUT);
+  pinMode(BTN_3, INPUT);
+  pinMode(BTN_4, INPUT);
+  pinMode(zoomPin, OUTPUT);
   disp1.clear();
-  disp1.brightness(7);  // яркость, 0 - 7 (минимум - максимум)
+  disp1.brightness(7);
   disp1.clear(); 
   digitalRead(BTN_1); 
   digitalRead(BTN_2);
   disp2.clear();
-  disp2.brightness(7);  // яркость, 0 - 7 (минимум - максимум)
+  disp2.brightness(7);
   disp2.clear(); 
 }
 
-void loop(){
-  clockbtn();
-  select_mode();
+void loop(){  
+  clockBtn();
+  selectMode();
   chessClock();
 }
 
-void chessClock(){
- if (config_mode == false){
-  disp1.displayClock(MIN, SEC);
-  disp1.point(1);
-  disp2.displayClock(MIN1, SEC1);
-  disp2.point(1);
-  if (MIN1 != 0 && SEC1 != 0){
- if (bt1 == true){
-  if (MIN != 0){
-    SEC--;
-    delay(1000);
-    if (SEC == 0){
-      SEC = 59;
-      MIN--;
+void chessClock() {
+  if (configMode == false){
+    disp1.displayClock(MIN, SEC);
+    disp1.point(1);
+    disp2.displayClock(MIN1, SEC1);
+    disp2.point(1);
+  if (MIN1 != 0 && SEC1 != 0) {
+    if (bt1 == true) {
+      if (MIN != 0) {
+        SEC--;
+        delay(1000);
+        if (SEC == 0) {
+          SEC = 59;
+          MIN--;
     }
   }
   else {
     SEC--;
     delay(1000);
-    if (MIN == 0 && SEC == 0){
-      disp1.displayClock(finish_min, finish_sec);
-      digitalWrite(zum, HIGH);
+    if (MIN == 0 && SEC == 0) {
+      disp1.displayClock(finishMin, finishSec);
+      digitalWrite(zoomPin, HIGH);
       delay(500);
-      digitalWrite(zum, LOW);
+      digitalWrite(zoomPin, LOW);
       clockPoint2();
     }
   }
- }
- else{
-  disp1.displayClock(MIN, SEC);
- }
+  }
+  else {
+   disp1.displayClock(MIN, SEC);
+  }
  }
  if (MIN != 0 && SEC != 0){
  if (bt2 == true){
@@ -92,10 +97,10 @@ void chessClock(){
     SEC1--;
     delay(1000);
     if (MIN1 == 0 && SEC1 == 0){
-      disp2.displayClock(finish_min, finish_sec);
-      digitalWrite(zum, HIGH);
+      disp2.displayClock(finishMin, finishSec);
+      digitalWrite(zoomPin, HIGH);
       delay(500);
-      digitalWrite(zum, LOW);
+      digitalWrite(zoomPin, LOW);
       clockPoint2();
     }
   }
@@ -120,13 +125,13 @@ void clockPoint2(){
   delay(1000);
   disp2.point(1);
 }
-void clockbtn() {
+void clockBtn() {
   if (digitalRead(BTN_4) == HIGH){
       SEC = SEC + 30;
       delay(200);
     }
   if (digitalRead(BTN_3) == HIGH){
-    config_mode = false;
+    configMode = false;
   }
 if (digitalRead(BTN_1) == HIGH){
     bt1 = true;
@@ -137,15 +142,15 @@ if (digitalRead(BTN_1) == HIGH){
     bt2 = true;
   }
 }
-void select_mode(){
+void selectMode() {
   disp1.displayClock(MIN, SEC);
-  if (config_mode == true) {
+  if (configMode == true) {
     
     if (SEC == 60){
       SEC = 0;
       MIN++;
     }
-    MIN1 = MIN + 0;
-    SEC1 = SEC + 0;
+    MIN1 = MIN;
+    SEC1 = SEC;
   }
 }
